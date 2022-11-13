eb885ac7f84e94b983ddb6ff953fdfc27780609c
Mefiasty <freshminter@gmail.com>
2022-11-13 17:16:00 +0100

    adding github link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/header-img.png';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-import '../css/header.css';
+import logo from "../assets/img/header-img.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../css/header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Header = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -22,16 +21,16 @@ export const Header = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Router>
@@ -45,23 +44,60 @@ export const Header = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Główna</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Umięjętności</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projekty</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Główna
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Umięjętności
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projekty
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/mateusz-harbuz-a74043256"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/mateusz.harbuz/"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/mefiakk/?hl=af"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/mateusz-harbuz-a74043256">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="https://www.facebook.com/mateusz.harbuz/">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="https://www.instagram.com/mefiakk/?hl=af">
+                  <img src={navIcon3} alt="" />
+                </a>
+                <a href="https://github.com/Mefiasty/mateuszharbuz.github.io">
+                  <FontAwesomeIcon icon="fa-brands fa-github" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Skontaktuj Się</span></button>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Skontaktuj Się</span>
+                </button>
               </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
